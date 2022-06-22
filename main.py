05789d2c5702c8164746dbddb5caa113fdc71215
史卓洲 <zhuozhou.shi@longbridge.sg>
2022-06-22 22:09:13 +0800

    [新增] - 创建基础结构

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.exception_handlers import HTTPException, RequestValidationError
+
+from core.events import startup, shutdown
+from core.exception import http_error_handler, http422_error_handler, unicorn_exception_handler, UnicornException
 
 app = FastAPI()
+
+
+app.add_event_handler("startup", startup(app))
+app.add_event_handler("shutdown", shutdown(app))
+
+app.add_exception_handler(HTTPException, http_error_handler)
+app.add_exception_handler(RequestValidationError, http422_error_handler)
+app.add_exception_handler(UnicornException, unicorn_exception_handler)
 
 
 @app.get("/")
--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
-# Readme
+# FastApi
+
+### 路由/视图
+
+* APIRouter
+* Incloude_router
+* Jinja2
+
+### 安全
+* JWT
+* RBAC
+* Depends
+* Exeception
+
+### 中间件
+
+* Cookies
+* Session
+* Request
+* Response
+
+### 第三方库
+
+* 短信
+* 支付（微信/支付宝）
+* 数据库（mysql/pgsql）
+* 缓存（File、Redis）
